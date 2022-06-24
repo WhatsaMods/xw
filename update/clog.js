6f54257dbc009fcd74638843f03f5e44d5475a57
WhatsaMods <whatsamods@gmail.com>
2022-06-24 17:08:42 +0200

    update

--- update/clog.js
+++ update/clog.js
@@ -1,10 +1,10 @@
 {
 	"packages": [
 		{	
-			"version": "2.22.14.11",
+			"version": "2.22.14.14",
 			"package": "com.xwasha",
 			"title": "XWasha",
-			"download": "https://github.com/WhatsaMods/xw/raw/master/bt/x2.22.14.11Beta.apk",
+			"download": "https://github.com/WhatsaMods/xw/raw/master/bt/x2.22.14.14Beta.apk",
 			"update": [
 				{
 					"languaje": "es",
@@ -29,10 +29,10 @@
 			]
 		},
 		{
-			"version": "2.22.14.11",
+			"version": "2.22.14.14",
 			"package": "com.whatsapp",
 			"title": "WhatsApp",
-			"download": "https://github.com/WhatsaMods/xw/raw/master/bt/w2.22.14.11Beta.apk",
+			"download": "https://github.com/WhatsaMods/xw/raw/master/bt/w2.22.14.14Beta.apk",
 			"update": [
 				{
 					"languaje": "es",
