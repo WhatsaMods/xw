181608df028215b905b401bd16fc1cf72115095c
WhatsaMods <whatsamods@gmail.com>
2023-08-18 18:26:06 +0200

    update

---
+++ update/xtable.js
@@ -0,0 +1,60 @@
+{
+	"packages": [
+		{	
+			"version": "2.23.16.77",
+			"package": "com.xwasha",
+			"title": "XWasha",
+			"download": "https://github.com/WhatsaMods/xw/raw/master/st/x2.23.16.77.apk",
+			"update": [
+				{
+					"languaje": "es",
+					"theme": "dark",
+					"changelog": "https://raw.githubusercontent.com/WhatsaMods/xw/master/update/stable/mod-es-d.html"
+				},
+				{
+					"languaje": "en",
+					"theme": "dark",
+					"changelog": "https://raw.githubusercontent.com/WhatsaMods/xw/master/update/stable/mod-en-d.html"
+				},
+				{
+					"languaje": "es",
+					"theme": "light",
+					"changelog": "https://raw.githubusercontent.com/WhatsaMods/xw/master/update/stable/mod-es-l.html"
+				},
+				{
+					"languaje": "en",
+					"theme": "light",
+					"changelog": "https://raw.githubusercontent.com/WhatsaMods/xw/master/update/stable/mod-en-l.html"
+				}
+			]
+		},
+		{
+			"version": "2.23.16.77",
+			"package": "com.whatsapp",
+			"title": "WhatsApp",
+			"download": "https://github.com/WhatsaMods/xw/raw/master/st/w2.23.16.77.apk",
+			"update": [
+				{
+					"languaje": "es",
+					"theme": "dark",
+					"changelog": "https://raw.githubusercontent.com/WhatsaMods/xw/master/update/stable/mod-es-d.html"
+				},
+				{
+					"languaje": "en",
+					"theme": "dark",
+					"changelog": "https://raw.githubusercontent.com/WhatsaMods/xw/master/update/stable/mod-en-d.html"
+				},
+				{
+					"languaje": "es",
+					"theme": "light",
+					"changelog": "https://raw.githubusercontent.com/WhatsaMods/xw/master/update/stable/mod-es-l.html"
+				},
+				{
+					"languaje": "en",
+					"theme": "light",
+					"changelog": "https://raw.githubusercontent.com/WhatsaMods/xw/master/update/stable/mod-en-l.html"
+				}
+			]
+		}
+	]
+}
